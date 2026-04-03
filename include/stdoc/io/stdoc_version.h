5c841a8cd119384f9533a453d8e4ddb1af3699e0
Uoc Tamika <uocdev.github.io@gmail.com>
2026-04-03 22:30:53 +0000

    Update stdoc_version and update make for hash

--- include/stdoc/io/stdoc_version.h
+++ include/stdoc/io/stdoc_version.h
@@ -1,6 +1,20 @@
 #ifndef STDOC_VERSION_H
 #define STDOC_VERSION_H
 
+#define STDOC_VERSION_MAJOR 0
+#define STDOC_VERSION_MINOR 0
+#define STDOC_VERSION_PATCH 1
+#define STDOC_VERSION_STRING "0.0.1"
+
+/* These can be defined at compile time, e.g., -DSTDOC_BUILD_HASH="<git_hash>" */
+#ifndef STDOC_BUILD_HASH
+#define STDOC_BUILD_HASH "unknown"
+#endif
+
+#ifndef STDOC_BUILD_DATE
+#define STDOC_BUILD_DATE __DATE__ " " __TIME__
+#endif
+
 /*
  * stdoc_version - print library/version information
  *
--- io/stdoc_version.c
+++ io/stdoc_version.c
@@ -7,5 +7,9 @@
 void stdoc_version(void)
 {
     stdoc_printf("     standard library Uoc     \n");
-    stdoc_printf("Version: 0.0.1 \n");
+    stdoc_printf("Version: %s\n", STDOC_VERSION_STRING);
+    stdoc_printf("Major: %d, Minor: %d, Patch: %d\n",
+               STDOC_VERSION_MAJOR, STDOC_VERSION_MINOR, STDOC_VERSION_PATCH);
+    stdoc_printf("Build Hash: %s\n", STDOC_BUILD_HASH);
+    stdoc_printf("Build Date: %s\n", STDOC_BUILD_DATE);
 }
